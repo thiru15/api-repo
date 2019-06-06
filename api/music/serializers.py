3fb8fe01df4a6d1b3b237988f93bb5f080a14c54
thiru15 <33665749+thiru15@users.noreply.github.com>
2019-06-06 10:18:02 +0530

    Add files via upload

---
+++ api/music/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from music.models import Songs
+
+
+class SongsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Songs
+        fields = ("title", "artist")
---
+++ api/music/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from .models import Songs
+from music.serializers import SongsSerializer
+
+
+class ListSongsView(generics.ListAPIView):
+    """
+    Provides a get method handler.
+    """
+    queryset = Songs.objects.all()
+    serializer_class = SongsSerializer
+
+# Create your views here.
